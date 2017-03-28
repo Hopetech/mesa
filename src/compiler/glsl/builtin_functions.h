04df74fbc77a8854b7d164d40e64515c70edcdd0
Elie Tournier <tournier.elie@gmail.com>
2017-03-28 11:46:06 +0100

    glsl: Add "built-in" function to do abs(fp64)
    Signed-off-by: Elie Tournier <tournier.elie@gmail.com>

--- src/compiler/glsl/builtin_functions.h
+++ src/compiler/glsl/builtin_functions.h
@@ -63,6 +63,9 @@ umul64(void *mem_ctx, builtin_available_predicate avail);
 ir_function_signature *
 sign64(void *mem_ctx, builtin_available_predicate avail);
 
+ir_function_signature *
+fabs64(void *mem_ctx, builtin_available_predicate avail);
+
 }
 
 #endif /* BULITIN_FUNCTIONS_H */
