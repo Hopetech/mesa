1e134780bf2926f930720190eaedc608bca3f25f
Elie Tournier <tournier.elie@gmail.com>
2017-05-15 17:16:59 +0100

    st/mesa: enable optional d2i lowering
    Signed-off-by: Elie Tournier <tournier.elie@gmail.com>

--- src/mesa/state_tracker/st_glsl_to_tgsi.cpp
+++ src/mesa/state_tracker/st_glsl_to_tgsi.cpp
@@ -6945,7 +6945,8 @@ st_link_shader(struct gl_context *ctx, struct gl_shader_program *prog)
                                MUL64 |
                                DIV64 |
                                F2D |
-                               D2F;
+                               D2F |
+                               D2I;
          lower_64bit_double_instructions(ir, lower_inst);
       }
       if (ctx->Extensions.ARB_shading_language_packing) {
