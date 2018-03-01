a4a6fd52ba0f9dfd561f201c9ae0a2e009b0dcb2
Dave Airlie <airlied@redhat.com>
2018-03-02 09:21:58 +1000

    add rcp matrix lowering

--- src/compiler/glsl/lower_mat_op_to_vec.cpp
+++ src/compiler/glsl/lower_mat_op_to_vec.cpp
@@ -360,6 +360,7 @@ ir_mat_op_to_vec_visitor::visit_leave(ir_assignment *orig_assign)
    switch (orig_expr->operation) {
    case ir_unop_d2f:
    case ir_unop_f2d:
+   case ir_unop_rcp:
    case ir_unop_neg: {
       /* Apply the operation to each column.*/
       for (i = 0; i < matrix_columns; i++) {
